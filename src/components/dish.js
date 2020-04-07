8281595d134a6b0b39ac74710b7d0a931c3ca0e0
Andrii Koretskyi <andriiko@wix.com>
2020-04-07 21:30:25 +0300

    Add custom hooks

--- src/components/dish.js
+++ src/components/dish.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Button } from 'antd';
+import { useAmount } from '../hooks/use-amount';
 
 export default function Dish(props) {
+  const { count, decrement, increment } = useAmount();
+
   return (
     <div>
       <p>{props.dish.name}</p>
       <p>{props.dish.price}</p>
+      {count}
+      <Button type="primary" onClick={decrement}>
+        -
+      </Button>
+      <Button type="primary" onClick={increment}>
+        +
+      </Button>
     </div>
   );
 }
